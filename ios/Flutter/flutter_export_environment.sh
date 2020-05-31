1a5a273699c75212e54f5afe8cb7b3a99365d4c5
Alejandro <noxcraft123@gmail.com>
2020-05-31 20:33:29 +0200

    actualizacion sdk

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\ese_v\Desktop\PACHANYA\FlutterSDK\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ese_v\Objetive Proyecto\objetive"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\ese_v\Desktop\PACHANYA\FlutterSDK\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.9"
+export "FLUTTER_BUILD_NUMBER=9"
