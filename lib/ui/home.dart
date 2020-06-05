67600aac982d674849d390280ddaf419e82fb318
Alejandro <noxcraft123@gmail.com>
2020-06-05 08:01:11 +0200

    Añadido notificaciones, pendiente de estilos

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ThreeObjective/models/nodo_item.dart';
 import 'package:ThreeObjective/utils/database_utils.dart';
 import 'package:ThreeObjective/utils/date_formatter.dart';
 import 'package:ThreeObjective/ver_objetivo.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -25,6 +29,8 @@ class _HomeState extends State<Home> {
   var db = new DatabaseHelper();
   final List<ItemObjetivo> itemList = <ItemObjetivo>[];
 
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
   //DatePicker
   final FocusNode _focusNodeFecha = FocusNode();
   final FocusNode _focusNodeFechaBuscar = FocusNode();
@@ -42,7 +48,9 @@ class _HomeState extends State<Home> {
           lastDate: DateTime(DateTime.now().year + 5));
       _focusNodeFecha.unfocus();
       setState(() {
+        print("s.o.s");
         if (picked != null) {
+          print("s.o.s2");
           selectedDate = picked;
           itemControllerFecha.text = parseFecha(selectedDate);
         }
@@ -50,6 +58,25 @@ class _HomeState extends State<Home> {
     }
   }
 
+  Future scheuleAtParticularTime(DateTime timee) async {
+    var time = Time(timee.hour, timee.minute, timee.second);
+    print(time.toString());
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'repeatDailyAtTime channel id',
+        'repeatDailyAtTime channel name',
+        'repeatDailyAtTime description');
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    flutterLocalNotificationsPlugin.showDailyAtTime(0, 'ThreeObjective',
+        'Recuerda dedicarle un tiempo a tus objetivos.', time, platformChannelSpecifics);
+  }
+
+  Future<String>_obtenerEstado() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String contieneDatosPref = prefs.getBool('estado').toString() ?? null;
+    return contieneDatosPref;
+  }
   //DatePickerBuscar
   Future<Null> _selectorFechaBuscar(BuildContext context) async {
     if (_focusNodeFechaBuscar.hasFocus) {
@@ -91,7 +118,29 @@ class _HomeState extends State<Home> {
         _selectorFechaBuscar(context);
       });
     }
-  }
+
+    //Comprobamos si es la primera vez que se hace las notificaciones para activarlas por defecto
+    _obtenerEstado().then((value){
+        if((value == null || value == "null")){
+          print("BLABLA: $value");
+          var initializationSettingsAndroid =
+          new AndroidInitializationSettings('@mipmap/ic_launcher');
+          var initializationSettingsIOS = new IOSInitializationSettings();
+          var initializationSettings = new InitializationSettings(
+              initializationSettingsAndroid, initializationSettingsIOS);
+
+          flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+          flutterLocalNotificationsPlugin.initialize(initializationSettings);
+          //Se establece las 17:0:0 por defecto.
+          DateTime date = DateTime(2020, 6, 4, 17, 0, 0);
+          print("activamos notificaciones por primera instancia: $date");
+          scheuleAtParticularTime(
+              DateTime.fromMillisecondsSinceEpoch(
+                  date.millisecondsSinceEpoch));
+
+        }
+      });
+}
 
   @override
   Widget build(BuildContext context) {
@@ -372,6 +421,7 @@ class _HomeState extends State<Home> {
   void volverPrincipal(_) {
       Navigator.pop(context);
   }
+
 
   void botonVolverCreacion(_) {
     itemControllerObjetivo.clear();
--- lib/ui/notificacion.dart
+++ lib/ui/notificacion.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:ThreeObjective/models/nodo_item.dart';
-import 'package:ThreeObjective/utils/database_utils.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 class Notificacion extends StatefulWidget {
   @override
@@ -8,72 +12,160 @@ class Notificacion extends StatefulWidget {
 }
 
 class _NotificacionState extends State<Notificacion> {
-  //Principal
-  var db = new DatabaseHelper();
-
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  bool isSwitched = false;
+  String hora = "";
 
   @override
-  void initState() {
+  initState() {
     super.initState();
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    // If you have skipped STEP 3 then change app_icon to @mipmap/ic_launcher
+    var initializationSettingsAndroid =
+    new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = new IOSInitializationSettings();
+    var initializationSettings = new InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+    //Carga el estado de las preferencias. (true por defecto)
+    _obtenerEstado().then((value){
+      setState(() {
+        isSwitched = value;
+      });
+    });
+    //Carga la hora seleccionada de las preferencias, 17:0:0 por defecto.
+    _obtenerHora().then((value){
+      setState(() {
+        hora = value;
+      });
+    });
+
 
   }
 
+  Future<bool>_obtenerEstado() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('estado') ?? true;
+  }
+
+  Future<String> _obtenerHora() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('horaAviso') ?? "17:0:0";
+  }
+
+  _guardarHoraEstado() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('estado', isSwitched);
+    await prefs.setString('horaAviso', hora);
+  }
+
+
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      backgroundColor: Colors.grey.shade100,
-      appBar: new AppBar(
-        title: Text("Histórico"),
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () => volverPrincipal(context),
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Recordatorio'),
         ),
-        flexibleSpace: Container(
-          decoration: new BoxDecoration(
-            gradient: new LinearGradient(
-                colors: [
-                  Colors.brown[400],
-                  Colors.brown[900],
-                ],
-                begin: const FractionalOffset(0.0, 0.7),
-                end: const FractionalOffset(1.0, 0.0),
-                stops: [0.0, 1.0],
-                tileMode: TileMode.clamp),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              new SizedBox(
+                height: 30.0,
+              ),
+              Switch(
+                value: isSwitched,
+                onChanged: (value) {
+                  setState(() {
+                    isSwitched = value;
+                    print(isSwitched);
+                    if(isSwitched){
+                      //Activamos notificaciones
+                      int h = int.parse(hora.split(":").elementAt(0));
+                      int m = int.parse(hora.split(":").elementAt(1));
+                      int s = int.parse(hora.split(":").elementAt(2));
+                      DateTime date = DateTime(2020, 6, 4, h, m, s);
+                      print("activamos notificaciones: $date");
+                      scheuleAtParticularTime(
+                          DateTime.fromMillisecondsSinceEpoch(
+                              date.millisecondsSinceEpoch));
+
+                    }else{
+                      // Desactivar notificaciones
+                      flutterLocalNotificationsPlugin.cancelAll();
+                    }
+                    _guardarHoraEstado();
+                  });
+                },
+                activeTrackColor: Colors.lightGreenAccent,
+                activeColor: Colors.green,
+              ),
+              new SizedBox(
+                height: 30.0,
+              ),
+              new Text(
+                '$hora'
+              ),
+              new SizedBox(
+                height: 30.0,
+              ),
+              FlatButton(
+                color: Colors.green,
+                  onPressed: () {
+                    DatePicker.showTimePicker(context, showTitleActions: true,
+                        onChanged: (date) {
+                          print('change $date');
+                        }, onConfirm: (date) {
+                          print('confirm $date');
+                          setState(() {
+                            hora =  date.hour.toString()+':'+date.minute.toString()+':'+date.second.toString();
+                            print(hora);
+                          });
+                          _guardarHoraEstado();
+                         if(isSwitched){
+                           scheuleAtParticularTime(
+                               DateTime.fromMillisecondsSinceEpoch(
+                                   date.millisecondsSinceEpoch));
+                         }
+                        }, currentTime: DateTime.now(), locale: LocaleType.en);
+                  },
+                  child: Text(
+                    'Selecciona la hora de recordatorio',
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.w900),
+                  )),
+            ],
           ),
         ),
-        centerTitle: true,
-      ),
-      body: new Column(
       ),
     );
   }
 
-  void volverPrincipal(_) {
-    Navigator.pop(context);
+  Future scheuleAtParticularTime(DateTime timee) async {
+    var time = Time(timee.hour, timee.minute, timee.second);
+    print(time.toString());
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        'repeatDailyAtTime channel id',
+        'repeatDailyAtTime channel name',
+        'repeatDailyAtTime description');
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    flutterLocalNotificationsPlugin.showDailyAtTime(0, 'ThreeObjective',
+        'Recuerda dedicarle un tiempo a tus objetivos.', time, platformChannelSpecifics);
+    print('scheduled');
+    Fluttertoast.showToast(
+        msg:
+        "Tus notificaciones fueron programadas: ${time.hour} : ${time.minute} : ${time.second}",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        // also possible "TOP" and "CENTER"
+        backgroundColor: Colors.grey,
+        textColor: Colors.white);
   }
-
-  Divider calculaColorDivider(List<ItemObjetivo> lista, index){
-
-    Color color =  Colors.transparent;
-    if(index + 1 < lista.length) {
-      if (lista[index].fechaRealizar != lista[index + 1].fechaRealizar) {
-        color = Colors.brown;
-      } else {
-        color = Colors.transparent;
-      }
-    }
-    Divider divider = new Divider(color: color);
-    return divider;
-  }
-
-
-
-
-  void volver(context) {
-    Navigator.pop(context);
-  }
-
-
 }
